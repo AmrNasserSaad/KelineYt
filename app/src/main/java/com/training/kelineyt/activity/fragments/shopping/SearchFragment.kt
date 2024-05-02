f24864e374940e5115f86e8c18ea1449a4bf07d3
AmrNasser <amr931111@gmail.com>
2024-05-02 16:20:36 +0300

    add some navigation between SearchFragment and Categories

--- app/src/main/java/com/training/kelineyt/activity/fragments/shopping/SearchFragment.kt
+++ app/src/main/java/com/training/kelineyt/activity/fragments/shopping/SearchFragment.kt
@@ -1,7 +1,48 @@
 package com.training.kelineyt.activity.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.training.kelineyt.R
+import com.training.kelineyt.databinding.FragmentSearchBinding
 
-class SearchFragment:Fragment(R.layout.fragment_search ) {
+class SearchFragment : Fragment() {
+    private lateinit var binding : FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navigateToCategory()
+
+    }
+
+    private fun navigateToCategory() {
+        binding.cardTable.setOnClickListener {
+            findNavController().navigate(R.id.action_searchFragment_to_tableFragment)
+        }
+        binding.cardAccessory.setOnClickListener {
+            findNavController().navigate(R.id.action_searchFragment_to_accessoryFragment)
+        }
+        binding.cardChair.setOnClickListener {
+            findNavController().navigate(R.id.action_searchFragment_to_chairFragment)
+        }
+        binding.cardCupboard.setOnClickListener {
+            findNavController().navigate(R.id.action_searchFragment_to_cupboardFragment)
+        }
+        binding.cardFurniture.setOnClickListener {
+            findNavController().navigate(R.id.action_searchFragment_to_furnitureFragment)
+        }
+    }
 }
